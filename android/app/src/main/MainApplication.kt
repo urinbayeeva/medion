e9c59d85fb84c1b648d32c4db4bf296378f0dc27
Feruza <urinbayeevaa@gmail.com>
2024-11-27 17:05:43 +0500

    yandex map was removed and replaced by google maps

--- android/app/src/main/MainApplication.kt
+++ android/app/src/main/MainApplication.kt
@@ -1,11 +1,11 @@
-import android.app.Application
+package com.example.PACKAGE_NAME
 
+import android.app.Application
 import com.yandex.mapkit.MapKitFactory
 
 class MainApplication: Application() {
   override fun onCreate() {
     super.onCreate()
-    // MapKitFactory.setLocale("YOUR_LOCALE")
-    MapKitFactory.setApiKey("3f886df1-7552-4a91-8dc7-3511e772780d") // Your generated API key
+    MapKitFactory.setApiKey("3f886df1-7552-4a91-8dc7-3511e772780d")
   }
 }
