696bf7a4c702b6b11c42d8eb320ab94c5dbecbed
Feruza <urinbayeevaa@gmail.com>
2025-03-18 17:25:43 +0500

    some changes

--- lib/application/profile/profile_bloc.dart
+++ lib/application/profile/profile_bloc.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:medion/presentation/component/easy_loading.dart';
 
 part 'profile_bloc.freezed.dart';
 part 'profile_event.dart';
@@ -22,7 +24,8 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
   }
 
   Future<void> _pickImage(_PickImage event, Emitter<ProfileState> emit) async {
-    final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
       final croppedFile = await _cropImage(File(pickedFile.path));
@@ -53,8 +56,11 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
     return croppedFile != null ? File(croppedFile.path) : null;
   }
 
-  Future<void> _uploadImage(_UploadImage event, Emitter<ProfileState> emit) async {
+  Future<void> _uploadImage(
+      _UploadImage event, Emitter<ProfileState> emit) async {
     if (state.pickedImagePath == null) return;
+
+    EasyLoading.show(status: "Uploading...".tr());
 
     try {
       File imageFile = File(state.pickedImagePath!);
@@ -66,7 +72,8 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
         options: Options(
           headers: {
             'accept': 'application/json',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNTIwNSIsImV4cCI6MTc0MjU1ODk4Mn0.hRFSKs8Db6sjv1hxJV25FnDSRjTYGMlW1mqWsP9XjSQ',
+            'Authorization':
+                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxNTIwNSIsImV4cCI6MTc0MjU1ODk4Mn0.hRFSKs8Db6sjv1hxJV25FnDSRjTYGMlW1mqWsP9XjSQ',
             'Content-Type': 'application/json',
           },
         ),
@@ -75,11 +82,16 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
 
       if (response.statusCode == 200) {
         print('Image uploaded successfully: ${response.data}');
+        EasyLoading.showSuccess("Upload Successful".tr());
       } else {
         print('Failed to upload image: ${response.statusCode}');
+        EasyLoading.showError("Upload Failed".tr());
       }
     } catch (e) {
       print('Error uploading image: $e');
+      EasyLoading.showError('Error uploading image');
+    } finally {
+      EasyLoading.dismiss(); // Hide loading indicator
     }
   }
 }
