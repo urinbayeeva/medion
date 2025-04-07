53bbe3946df470433fb425408e667dec5e53413d
Feruza <urinbayeevaa@gmail.com>
2025-04-07 17:13:52 +0500

    about doctor page UI design issues are fixed

--- lib/presentation/pages/home/doctors/inner_page/about_doctor.dart
+++ lib/presentation/pages/home/doctors/inner_page/about_doctor.dart
@@ -43,174 +43,209 @@ class _AboutDoctorState extends State<AboutDoctor> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DoctorBloc, DoctorState>(builder: (context, state) {
-      if (state.doctorDetails == null) {
-        return Center(child: CircularProgressIndicator());
-      }
-
-      final doctor = state.doctorDetails!;
-
-      return ThemeWrapper(builder: (context, colors, fonts, icons, controller) {
-        return DefaultTabController(
-          length: 4,
-          child: Scaffold(
-            backgroundColor: colors.backgroundColor,
-            body: Column(
-              children: [
-                CAppBar(
-                  title: "doctors".tr(),
-                  isBack: true,
-                  centerTitle: true,
-                  trailing: 24.w.horizontalSpace,
-                  bottom: Column(
-                    children: [
-                      AboutDoctorWidget(
-                        name: widget.name,
-                        profession: widget.profession,
-                        specialty: widget.status,
-                        image: widget.image,
-                      ),
-                      16.h.verticalSpace,
-                      CustomTabbarBlack(
-                        leftTab: "about_the_doctor".tr(),
-                        middleTab: "working_experience".tr(),
-                        nextMiddle: "education".tr(),
-                        rightTab: "working_hours".tr(),
-                      ),
-                    ],
-                  ),
+    return ThemeWrapper(builder: (context, colors, fonts, icons, controller) {
+      return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          backgroundColor: colors.backgroundColor,
+          body: Column(
+            children: [
+              CAppBar(
+                title: "doctors".tr(),
+                isBack: true,
+                centerTitle: true,
+                trailing: 24.w.horizontalSpace,
+                bottom: Column(
+                  children: [
+                    AboutDoctorWidget(
+                      name: widget.name,
+                      profession: widget.profession,
+                      specialty: widget.status,
+                      image: widget.image,
+                    ),
+                    16.h.verticalSpace,
+                    CustomTabbarBlack(
+                      leftTab: "about_the_doctor".tr(),
+                      middleTab: "working_experience".tr(),
+                      nextMiddle: "education".tr(),
+                      rightTab: "working_hours".tr(),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      // About the Doctor tab
-                      _buildAboutDoctorTab(doctor, colors, fonts),
+              ),
+              BlocBuilder<DoctorBloc, DoctorState>(
+                builder: (context, state) {
+                  if (state.doctorDetails == null) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-                      // Working Experience tab
-                      _buildExperienceTab(doctor, colors, fonts, icons),
+                  final doctor = state.doctorDetails!;
+                  return Expanded(
+                    child: TabBarView(
+                      children: [
+                        // About the Doctor tab
+                        _buildAboutDoctorTab(doctor, colors, fonts, icons),
 
-                      // Education tab
-                      _buildEducationTab(doctor, colors, fonts, icons),
+                        // Working Experience tab
+                        _buildExperienceTab(doctor, colors, fonts, icons),
 
-                      // Working Hours tab
-                      _buildWorkingHoursTab(doctor, colors, fonts),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+                        // Education tab
+                        _buildEducationTab(doctor, colors, fonts, icons),
+
+                        // Working Hours tab
+                        _buildWorkingHoursTab(doctor, colors, fonts),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        );
-      });
+        ),
+      );
     });
   }
 
-  Widget _buildAboutDoctorTab(ModelDoctor doctor, colors, fonts) {
+  Widget _buildAboutDoctorTab(
+      ModelDoctor doctor, dynamic colors, dynamic fonts, icons) {
     return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("about_the_doctor".tr(), style: fonts.regularSemLink),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Text("about_the_doctor".tr(), style: fonts.regularSemLink),
+          ),
           8.h.verticalSpace,
-          CContainer(
-              text: doctor.decodedDescription.replaceAll('\n', '').trim()),
-          24.h.verticalSpace,
-          // Text("services_and_prices".tr(), style: fonts.regularSemLink),
-          8.h.verticalSpace,
-          // _buildPriceList(doctor.priceList, colors, fonts),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: CContainer(
+                text: doctor.decodedDescription.replaceAll('\n', '').trim()),
+          ),
+          _buildExperienceTab(doctor, colors, fonts, icons),
+          _buildEducationTab(doctor, colors, fonts, icons),
+          _buildWorkingHoursTab(doctor, colors, fonts),
         ],
       ),
     );
   }
 
-  Widget _buildExperienceTab(ModelDoctor doctor, colors, fonts, icons) {
+  Widget _buildExperienceTab(
+      ModelDoctor doctor, dynamic colors, dynamic fonts, dynamic icons) {
     if (doctor.experience.isEmpty) {
-      // return _buildEmptyState(colors, fonts, icons);
+      return _buildEmptyState(colors, fonts, icons);
     }
 
     return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("working_experience".tr(), style: fonts.regularSemLink),
           8.h.verticalSpace,
           Column(
-            children: doctor.experience
-                .map((exp) => Padding(
-                      padding: EdgeInsets.only(bottom: 16.h),
-                      child: Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.all(16.w),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8.r),
-                            color: colors.shade0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(exp.title,
-                                style: fonts.mediumMain
-                                    .copyWith(fontWeight: FontWeight.bold)),
-                            Text(exp.date, style: fonts.smallMain),
-                            8.h.verticalSpace,
-                            Text(exp.description, style: fonts.regularMain),
-                          ],
-                        ),
-                      ),
-                    ))
-                .toList(),
+            children: doctor.experience.map((exp) {
+              // Format the date
+              final dateParts = exp.date.split(" - ");
+              final startDate = DateTime.parse(dateParts[0]);
+              final formattedStartDate =
+                  DateFormat('d MMM yyyy').format(startDate);
+
+              // If the date includes 'current', we leave the end date as 'current'
+              final endDate = dateParts.length > 1 && dateParts[1] == 'current'
+                  ? 'current'
+                  : DateFormat('d MMM yyyy')
+                      .format(DateTime.parse(dateParts[1]));
+
+              return Padding(
+                padding: EdgeInsets.only(bottom: 16.h),
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16.w),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.r),
+                      color: colors.shade0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(exp.title,
+                          style: fonts.xSmallText
+                              .copyWith(fontWeight: FontWeight.bold)),
+                      Text('$formattedStartDate - $endDate',
+                          style: fonts.xSmallText),
+                      8.h.verticalSpace,
+                      Text(exp.description, style: fonts.xSmallText),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildEducationTab(ModelDoctor doctor, colors, fonts, icons) {
+  Widget _buildEducationTab(
+      ModelDoctor doctor, dynamic colors, dynamic fonts, dynamic icons) {
     if (doctor.education.isEmpty) {
-      // return _buildEmptyState(colors, fonts, icons);
+      return _buildEmptyState(colors, fonts, icons);
     }
 
     return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("education".tr(), style: fonts.regularSemLink),
           8.h.verticalSpace,
           Column(
-            children: doctor.education
-                .map((edu) => Padding(
-                      padding: EdgeInsets.only(bottom: 16.h),
-                      child: Container(
-                        padding: EdgeInsets.all(8.w),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: colors.shade0,
-                            borderRadius: BorderRadius.circular(8.r)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(edu.title,
-                                style: fonts.mediumMain
-                                    .copyWith(fontWeight: FontWeight.bold)),
-                            Text(edu.date, style: fonts.smallMain),
-                            8.h.verticalSpace,
-                            Text(edu.description, style: fonts.regularMain),
-                          ],
-                        ),
-                      ),
-                    ))
-                .toList(),
+            children: doctor.education.map((edu) {
+              // Format the date
+              final dateParts = edu.date.split(" - ");
+              final startDate = DateTime.parse(dateParts[0]);
+              final formattedStartDate =
+                  DateFormat('d MMM yyyy').format(startDate);
+
+              // If the date includes 'current', we leave the end date as 'current'
+              final endDate = dateParts.length > 1 && dateParts[1] == 'current'
+                  ? 'current'
+                  : DateFormat('d MMM yyyy')
+                      .format(DateTime.parse(dateParts[1]));
+
+              return Padding(
+                padding: EdgeInsets.only(bottom: 16.h),
+                child: Container(
+                  padding: EdgeInsets.all(8.w),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: colors.shade0,
+                      borderRadius: BorderRadius.circular(8.r)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(edu.title,
+                          style: fonts.xSmallLink
+                              .copyWith(fontWeight: FontWeight.bold)),
+                      Text('$formattedStartDate - $endDate',
+                          style: fonts.xSmallLink),
+                      8.h.verticalSpace,
+                      Text(edu.description, style: fonts.xSmallLink),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildWorkingHoursTab(ModelDoctor doctor, colors, fonts) {
+  Widget _buildWorkingHoursTab(
+      ModelDoctor doctor, dynamic colors, dynamic fonts) {
     return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -222,7 +257,7 @@ class _AboutDoctorState extends State<AboutDoctor> {
     );
   }
 
-  Widget _buildSchedule(WorkSchedule schedule, colors, fonts) {
+  Widget _buildSchedule(WorkSchedule schedule, dynamic colors, dynamic fonts) {
     final days = [
       {'day': 'Monday', 'schedules': schedule.monday},
       {'day': 'Tuesday', 'schedules': schedule.tuesday},
@@ -252,7 +287,7 @@ class _AboutDoctorState extends State<AboutDoctor> {
               padding: EdgeInsets.all(8.w),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8.r),
-                color: colors.neutral200,
+                color: Color(0xFFF9F9F9),
               ),
               child: Padding(
                 padding: EdgeInsets.only(bottom: 16.h),
@@ -260,16 +295,13 @@ class _AboutDoctorState extends State<AboutDoctor> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      // ignore: unnecessary_string_interpolations
-                      '${dayName.tr()}',
+                      dayName.tr(),
                       style: fonts.mediumMain.copyWith(
                         fontWeight: FontWeight.bold,
                         fontSize: 16.sp,
                         color: colors.primary900,
                       ),
                     ),
-
-                    // Schedule times
                     ...schedules
                         .map((item) => Text(
                               item.time,
@@ -289,72 +321,20 @@ class _AboutDoctorState extends State<AboutDoctor> {
     );
   }
 
-//   Widget _buildPriceList(BuiltList<PriceItem> priceList, colors, fonts) {
-//     // Filter out prices that are 0 or not relevant
-//     final filteredPrices = priceList
-//         .where((item) =>
-//             item.firstVisitPrice > 0 &&
-//             item.productType.isNotEmpty &&
-//             item.categId.isNotEmpty)
-//         .toList();
-
-//     if (filteredPrices.isEmpty) {
-//       return CContainer(text: "no_prices_available".tr());
-//     }
-
-//     return Column(
-//       children: filteredPrices
-//           .map((item) => Padding(
-//                 padding: EdgeInsets.only(bottom: 8.h),
-//                 child: Container(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text(item.productType,
-//                           style: fonts.mediumMain
-//                               .copyWith(fontWeight: FontWeight.bold)),
-//                       Text(item.categId, style: fonts.smallMain),
-//                       4.h.verticalSpace,
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text("first_visit".tr(), style: fonts.regularMain),
-//                           Text("${item.firstVisitPrice}",
-//                               style: fonts.regularMain),
-//                         ],
-//                       ),
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text("revisit".tr(), style: fonts.regularMain),
-//                           Text("${item.revisitPrice}",
-//                               style: fonts.regularMain),
-//                         ],
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ))
-//           .toList(),
-//     );
-//   }
-
-//   Widget _buildEmptyState(colors, fonts, icons) {
-//     return SizedBox(
-//       height: MediaQuery.of(context).size.height * 0.7,
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           // icons.emojiSad.svg(width: 80.w, height: 80.h),
-//           Center(
-//             child: Text(
-//               "no_result_found".tr(),
-//               style: fonts.mediumMain.copyWith(fontWeight: FontWeight.w600),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  Widget _buildEmptyState(dynamic colors, dynamic fonts, dynamic icons) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.7,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "no_result_found".tr(),
+              style: fonts.mediumMain.copyWith(fontWeight: FontWeight.w600),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
